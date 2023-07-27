c2ad0d77fd7b3dd66bc23e37c60f68b01bc0f0b4
Gowrav Tata <54976985+GowravTata@users.noreply.github.com>
2023-07-27 14:28:09 +0000

    Added config for gitlab

---
+++ scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Clone the repository
+echo "Checking for existing dataflow-automation folders, recreating the dataflow-automation repo"
+if [ -d ~/record_creation_flask_app/ ]; then
+  rm -rf record_creation_flask_app
+fi
+
+# Cloning the repository
+git clone git@gitlab.com:ltts_members/record_creation_flask_app.git
+
+#Changing  into Home Directory
+cd ~/record_creation_flask_app
+
+# Checking out the branch based on CI_COMMIT_REF_NAME
+GIT_BRANCH=$(git symbolic-ref --short HEAD)
+BRANCH=${1:-$GIT_BRANCH}
+echo "⚙ Checking out Record Creation Flask App branch to ${BRANCH}"
+git checkout --track origin/"${BRANCH}" || true
+
+# Run the Script to run the docker
+chmod +x run.sh
+./run.sh
