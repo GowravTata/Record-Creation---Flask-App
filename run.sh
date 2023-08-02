198455cae9531dc8d75fdccd174268d9d03f71ca
Gowrav Tata <54976985+GowravTata@users.noreply.github.com>
2023-08-02 12:48:42 +0000

    Added changes for the deployment

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# This runs the commands for startup 
-docker-compose up -d 
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,23 +3,23 @@
 set -e
 
 # Clone the repository
-echo "Checking for existing record_creation_flask_app folder , recreating the record_creation_flask_app repo"
-if [ -d ~/record_creation_flask_app/ ]; then
-  rm -rf record_creation_flask_app
+echo "Checking for existing Record-Creation---Flask-App folder , recreating the Record-Creation---Flask-App repo"
+if [ -d ~/Record-Creation---Flask-App/ ]; then
+  rm -rf Record-Creation---Flask-App
 fi
 
 # Cloning the repository
-git clone git@gitlab.com:ltts_members/record_creation_flask_app.git
+git clone https://github.com/GowravTata/Record-Creation---Flask-App.git
 
 #Changing  into Home Directory
-cd ~/record_creation_flask_app
+cd Record-Creation---Flask-App/
 
+# Use the below block only when the name of the branch is passed
 # Checking out the branch based on CI_COMMIT_REF_NAME
 GIT_BRANCH=$(git symbolic-ref --short HEAD)
 BRANCH=${1:-$GIT_BRANCH}
 echo "⚙ Checking out Record Creation Flask App branch to ${BRANCH}"
 git checkout --track origin/"${BRANCH}" || true
 
-# Run the Script to run the docker
-chmod +x run.sh
-./run.sh
+# Build the Docker file
+sudo docker-compose up -d --build
